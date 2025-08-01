3c1d5a84d730e0bb7808db5d9513ab6a5c26e381
rovijakhapre <rovija.khapre@gmail.com>
2025-08-01 08:05:46 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+USERNAME = "rovi"
+TOKEN = "seventeen"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor" : "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
